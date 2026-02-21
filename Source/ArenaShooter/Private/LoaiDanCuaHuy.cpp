c0ffa9b649bec6a7c130fcd48389b8e02c36c1a0
HuyNguyen164 <nghoanganhhuy164@gmail.com>
2026-02-21 17:26:20 +0700

    Update Đổi màu object khi bắn

--- Source/ArenaShooter/Private/LoaiDanCuaHuy.cpp
+++ Source/ArenaShooter/Private/LoaiDanCuaHuy.cpp
@@ -3,12 +3,39 @@
 
 #include "LoaiDanCuaHuy.h"
 
+#include "GameFramework/Character.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "Materials/MaterialInstanceDynamic.h"
+
 // Sets default values
 ALoaiDanCuaHuy::ALoaiDanCuaHuy()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Create components
+	SphereCollision = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollision"));
+	RootComponent = SphereCollision;
+	
+	SphereCollision->InitSphereRadius(15.f);
+	SphereCollision->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	SphereCollision->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
+	SphereCollision->SetCollisionObjectType(ECollisionChannel::ECC_PhysicsBody);
+	// SphereCollision->SetCollisionProfileName(TEXT("BlockAllDynamic"));
+	// SphereCollision->SetNotifyRigidBodyCollision(true);
+	SphereCollision->OnComponentHit.AddDynamic(this, &ALoaiDanCuaHuy::OnProjectileHit);
+	
+	// Set up collision properties for the sphere
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMesh->SetupAttachment(SphereCollision);
+	
+	// Set up projectile movement component
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+	ProjectileMovement->InitialSpeed = 6000.f;
+	ProjectileMovement->MaxSpeed = 6000.f;
+	ProjectileMovement->bRotationFollowsVelocity = true;
+	ProjectileMovement->bShouldBounce = true;
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +43,29 @@ void ALoaiDanCuaHuy::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void ALoaiDanCuaHuy::OnProjectileHit(
+	UPrimitiveComponent* HitComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse,
+	const FHitResult& Hit)
+{
+	ACharacter* HitCharacter = Cast<ACharacter>(OtherActor);
+	if (HitCharacter)
+	{
+		USkeletalMeshComponent* MeshComponent = HitCharacter->GetMesh();
+		if (MeshComponent)
+		{
+			UMaterialInterface* Material = MeshComponent->GetMaterial(0);
+			if (Material)
+			{
+				MeshComponent->CreateAndSetMaterialInstanceDynamic(0)->SetVectorParameterValue(
+					TEXT("Basecolor"), FLinearColor::Green);
+			}
+		}
+	}
 }
 
 // Called every frame
--- Source/ArenaShooter/Public/LoaiDanCuaHuy.h
+++ Source/ArenaShooter/Public/LoaiDanCuaHuy.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 #include "LoaiDanCuaHuy.generated.h"
 
 UCLASS()
@@ -14,10 +17,28 @@ class ARENASHOOTER_API ALoaiDanCuaHuy : public AActor
 public:	
 	// Sets default values for this actor's properties
 	ALoaiDanCuaHuy();
+	
+	UFUNCTION()
+	void OnProjectileHit(
+		UPrimitiveComponent* HitComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		FVector NormalImpulse,
+		const FHitResult& Hit
+	);
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = SphereCollision)
+	USphereComponent* SphereCollision;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = StaticMesh)
+	UStaticMeshComponent* StaticMesh;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = StaticMesh)
+	UProjectileMovementComponent* ProjectileMovement;
 
 public:	
 	// Called every frame
