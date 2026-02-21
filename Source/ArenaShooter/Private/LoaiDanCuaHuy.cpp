f944328b80b82d5a762ec785737369ac8bbccf52
HuyNguyen164 <nghoanganhhuy164@gmail.com>
2026-02-21 13:22:35 +0700

    Create Actor LoaiDanCuaHuy

---
+++ Source/ArenaShooter/Private/LoaiDanCuaHuy.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LoaiDanCuaHuy.h"
+
+// Sets default values
+ALoaiDanCuaHuy::ALoaiDanCuaHuy()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALoaiDanCuaHuy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALoaiDanCuaHuy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
